6b900bde977f04265ad378df855ea7495806fd78
italo <italo@pop-os.localdomain>
2022-04-03 18:49:42 -0300

    update

--- util/random.go
+++ util/random.go
@@ -1,6 +1,7 @@
 package util
 
 import (
+	"fmt"
 	"math/rand"
 	"strings"
 	"time"
@@ -8,19 +9,20 @@ import (
 
 const alphabet = "abcdefghijklmnopqrstuvwxyz"
 
-func init() { // inicia automaticamente quando o pacote e usado pela primeira vez
-	rand.Seed(time.Now().UnixMicro())
+func init() {
+	rand.Seed(time.Now().UnixNano())
 }
 
-// RandomInt: generate a random interger between min and max
+// RandomInt generates a random integer between min and max
 func RandomInt(min, max int64) int64 {
 	return min + rand.Int63n(max-min+1)
 }
 
-// RanodmString: generate a random string of length n
+// RandomString generates a random string of length n
 func RandomString(n int) string {
 	var sb strings.Builder
 	k := len(alphabet)
+
 	for i := 0; i < n; i++ {
 		c := alphabet[rand.Intn(k)]
 		sb.WriteByte(c)
@@ -29,19 +31,24 @@ func RandomString(n int) string {
 	return sb.String()
 }
 
-// RandomOwner: generate a random owner name
+// RandomOwner generates a random owner name
 func RandomOwner() string {
 	return RandomString(6)
 }
 
-// RandomMoney: generates a random amount of money
+// RandomMoney generates a random amount of money
 func RandomMoney() int64 {
 	return RandomInt(0, 1000)
 }
 
-// RandomCurrency: generates a random currency
+// RandomCurrency generates a random currency code
 func RandomCurrency() string {
-	currencies := []string{"EUR", "USD", "CAD", "BRL"}
+	currencies := []string{USD, EUR, CAD}
 	n := len(currencies)
 	return currencies[rand.Intn(n)]
+}
+
+// RandomEmail generates a random email
+func RandomEmail() string {
+	return fmt.Sprintf("%s@email.com", RandomString(6))
 }
